432a3e83de0ea10f4b7dc6bd3ee7b4fef41c8753
gtsdev <kharrison@gtsservices.ca>
2019-01-21 12:31:15 -0500

    working collapse expand button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,32 @@
+//burger menu function
+(function () {
+    var burger = document.querySelector('.burger');
+    var nav = document.querySelector('#'+burger.dataset.target);
 
+    console.log(burger);
+    console.log(nav);
 
-var msg = 'hello world';
+    burger.addEventListener('click', function(){
+        burger.classList.toggle('is-active');
+        nav.classList.toggle('is-active');
+    });
+})();
 
-console.log(msg);
+//jquery for hiding content
+
+$(".serviceList").click(function () {
+
+    $serviceList = $(this);
+    //getting the next element
+    $dryerContent = $serviceList.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $dryerContent.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of s$serviceList based on visibility of content div
+        $serviceList.text(function () {
+            //change text based on condition
+            return $dryerContent.is(":visible") ? "Collapse" : "Expand";
+        });
+    });
+
+});
