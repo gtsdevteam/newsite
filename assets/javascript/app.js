29702ddeaea5040aa133f85072b822a9d039e0e1
gtsdev <kharrison@gtsservices.ca>
2019-01-21 14:12:12 -0500

    hvac collapsing content jquery

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -25,7 +25,26 @@ $(".serviceList").click(function () {
         //change text of s$serviceList based on visibility of content div
         $serviceList.text(function () {
             //change text based on condition
-            return $dryerContent.is(":visible") ? "Collapse" : "Expand";
+            // return $dryerContent.is(":visible") ? "Collapse" : "Expand";
+        });
+    });
+
+});
+
+//hvac repair hidden content
+
+$(".hvacList").click(function () {
+
+    $.hvacList = $(this);
+    //getting the next element
+    $hvacContent = $.hvacList.next();
+    //open up the content needed - toggle the slide- if visible, slide up, if not slidedown.
+    $hvacContent.slideToggle(500, function () {
+        //execute this after slideToggle is done
+        //change text of s$.hvacList based on visibility of content div
+        $.hvacList.text(function () {
+            //change text based on condition
+            // return $hvacContent.is(":visible") ? "Collapse" : "Expand";
         });
     });
 
